28b4b82bcdab0fdef0a0eab2157329c896f7d05b
avingtier <avingtier@gmail.com>
2019-01-06 18:43:53 +0100

    test deploy script

--- docker/connect.sh
+++ docker/connect.sh
@@ -2,4 +2,4 @@
 CONTAINER_NAME=$1
 CONTAINER_NAME=${CONTAINER_NAME:=php}
 echo -e "\e[32m> Connecting to your container ...\e[0m"
-gnome-terminal -x bash -c "sudo docker exec -ti $CONTAINER_NAME /bin/bash"
+command -v gnome-terminal && gnome-terminal -x bash -c "sudo docker exec -ti $CONTAINER_NAME /bin/bash" || sudo docker exec -ti ${CONTAINER_NAME} /bin/bash
