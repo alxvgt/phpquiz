d90e54f875742677a5726ebf6881b4725147d691
avingtier <avingtier@gmail.com>
2019-01-06 17:50:55 +0100

    test deploy script

--- app/Resources/deploy/deploy.sh
+++ app/Resources/deploy/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 source .deploy.env
+mkdir -p ~/.ssh
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 test -d ${SERVER_APP_PATH} && (cd ${SERVER_APP_PATH} && git checkout master && git reset --hard origin/master && git pull) || git clone git@github.com:alxvgt/phpquiz.git ${SERVER_APP_PATH}
 cp -f .env ${SERVER_APP_PATH}/.env
 cp -f .deploy.env ${SERVER_APP_PATH}/.deploy.env
