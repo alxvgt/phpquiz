b8af347cf256f21a8fe5e395995374a134455bb6
avingtier <avingtier@gmail.com>
2019-01-10 13:51:47 +0100

    update deploy paths on travis jobs

--- app/Resources/deploy/deploy.sh
+++ app/Resources/deploy/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-source .deploy.env
+source phpquiz/.deploy.env
 mkdir -p ~/.ssh
 ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 test -d ${SERVER_APP_PATH} && (cd ${SERVER_APP_PATH} && git checkout master && git reset --hard origin/master && git pull) || git clone git@github.com:alxvgt/phpquiz.git ${SERVER_APP_PATH}
-cat .deploy.env .env > ${SERVER_APP_PATH}/.env
+cat phpquiz/.deploy.env phpquiz/.env > ${SERVER_APP_PATH}/.env
 cd ${SERVER_APP_PATH}
 bash docker/start.sh
 docker exec php /bin/bash -c 'bash install.sh'
