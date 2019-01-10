d3c047a13a74c95a1c7b1dd8ed9ec6af19587ade
avingtier <avingtier@gmail.com>
2019-01-10 12:27:42 +0100

    debug travis

--- install.sh
+++ install.sh
@@ -29,6 +29,6 @@ crontab ./app/Resources/server/cron/crontab
 crontab -l
 
 echo -e "\e[32m> Creating database ...\e[0m"
-cat .env
-sudo apt-get update && sudo apt-get install -y netcat && nc -zv database 3306
+env
+apt-get update && apt-get install -y netcat && nc -zv database 3306
 php bin/console doctrine:database:create --if-not-exists
